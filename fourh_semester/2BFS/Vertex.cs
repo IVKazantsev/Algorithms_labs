d29e8eb06ba20fca701db57f9b0f5d0723ec37a3
IVKazantsev <97786849+IVKazantsev@users.noreply.github.com>
2023-02-11 11:29:08 +0200

    Add files via upload

---
+++ fourh_semester/2BFS/Vertex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BFS
+{
+    class Vertex
+    {
+        public List<Vertex> Vertexes = new List<Vertex>();
+        private int number;
+
+        public Vertex(int Number)
+        {
+            number = Number;
+        }
+
+
+        public int GetNumber()
+        {
+            return number;
+        }
+        public void AddVertex(Vertex vertex)
+        {
+            Vertexes.Add(vertex);
+        }
+    }
+}
